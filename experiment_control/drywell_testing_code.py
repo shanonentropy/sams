5adffd7e69464bb7448ebf3388bbc49c15180740
Ahmed <zahmed@NIST.GOV>
2023-10-22 09:58:05 -0400

    oop wait module

--- experiment_control/drywell_testing_code.py
+++ experiment_control/drywell_testing_code.py
@@ -18,22 +18,23 @@ as intended and catching the non-sensical out of
 #import modules
 import time
 from time import sleep
+from datetime import datetime, date
 #from waiting import wait
 import numpy as np
 from pathlib import Path
 import os # Import os module
 import pandas as pd
-
-
+import csv
+import pyvisa
 import sys # Import python sys module
 sys.path.append('c:\\sams\instrument_control')
 from drywell_interface import Dry_well # drywell control module
 from temperature_generator import Cycling
-from wait_for import wait_for_x
+from wait_for import wait_for_x, wait_for_drywell
 
 
-########## wait module 
-def wait_for_drywell(sleep_seconds = 30, timeout_seconds= 3000):
+########## wait module -  commented out
+'''def wait_for_drywell(sleep_seconds = 30, timeout_seconds= 3000):
     count = 1; 
     print('starting counter:', count); 
     to = time.monotonic() 
@@ -52,8 +53,8 @@ def wait_for_drywell(sleep_seconds = 30, timeout_seconds= 3000):
         else:
             print('timed out')
             break
-
-####### set drywell 
+'''
+####### instantiate the drywell 
 
 drywell = Dry_well()
 print(drywell.read_stability_status())
@@ -73,13 +74,9 @@ print(drywell.read_rate())
 
 
 
-
-''' note to self: does the drywell.read_rate() output require further processing
-before being inserted into a file name, if so, address it in ramp_test function 
-of the data_acq_pl file'''
-
 # set up a temp index
-temp_index = Cycling(start=25.0, stop=30.0, step=5, cycles=1).temperatures()
+print(Cycling(start=25.0, stop=35.0, step=5, cycles=1).params())
+temp_index = Cycling(start=25.0, stop=35.0, step=5, cycles=1).temperatures()
 
 for _, temperature in enumerate(temp_index):
     print(temperature)
@@ -92,14 +89,103 @@ for _, temperature in enumerate(temp_index):
     print('temperature is now stable at {}, moving to next temp'.format(drywell.read_temp()))
     
     
-print('testing_imported_module')
+print('testing_imported_module, setting temp to 27 C')
+drywell.set_temp(27)
+wait_for_x(drywell.read_stability_status(), sleep_seconds=30, timeout_seconds=3000)
+print('testing_imported_module, setting temp to 25 C')
 drywell.set_temp(25)
 wait_for_x(drywell.read_stability_status(), sleep_seconds=30, timeout_seconds=3000)
 
 
 
+#######################
 
-    
+''' here we will cycle the drywell 3 times over the temp range of interest
+while recording the temperature in the well using a check thermometer
+
+We will measure the cycle over the same range and conditions that we intend to 
+do most of our measurements on: -30 C to 70 C
+
+'''
+
+
+# instatiate the grid
+print(Cycling(start=-30.0, stop=70.0, step=5, cycles=3).params())
+temp_index = Cycling(start=-30.0, stop=70.0, step=5, cycles=3).temperatures()
+
+#instantiate the check thermometer
+
+
+## Connect to the multimeter
+rm = pyvisa.ResourceManager()
+dmm = rm.open_resource("GPIB0::21::INSTR") # check to see if this is true
+
+## Set the measurement
+
+#rest the device
+dmm.write("*RST")
+#configure to measure resistnace/voltage
+dmm.write("*RST")  #dmm.write(":CONF:VOLT:DC")
+# set range to auto
+dmm.write(":RES:RANG:AUTO:ON")
+#set the integration time to 1 sec for resistance/ for voltage 10 cycles
+dmm.write(":RES:APER 1") #dmm.write(":CONF:VOLT:DC")
+# set source trig to immediate
+dmm.write(":TRIG:SOUR IMM")
+#set num of readings to 5
+dmm.write(":SAMP:COUN 5")
+# take the readings 
+dmm.write(":SAMP:COUNT:AUTO ONCE")
+dmm.write(":FORM:ELEM READ")
+# put readings into a container
+a = np.fromstring((dmm.query(":READ?")).replace('\n',','), sep=',').mean()
+print('the mean resistance value is {}'.format(a))
+
+
+
+#### let's get cycling
+
+
+''' note: there is no extra settling time here; we are going to log
+thermometer readings immediately and keep logging them for 60 mins.
+the idea is to see how long it takes to get to equilibrium at each temperature
+over the range of interest and once at equilibrium does it stay stable?'''
+
+# lists to log data in
+time_of_measurement, set_temp, resistance = [],[],[] 
+
+for _, temperature in enumerate(temp_index):
+    print(temperature)
+    drywell.set_temp(temperature)
+    wait_for_x(sleep_seconds =30, timeout_seconds=2000)
+    drywell.beep()
+    t_start = time.monotonic()
+    while time.monotonic()-t_start<36:
+        a = np.fromstring((dmm.query(":READ?")).replace('\n',','), sep=',').mean()
+        time_of_measurement.append(time.monotonic(), temperature,resistance.append(a) )
+        sleep(10)
+        print('coco')
+        
+        
+### save file
+
+data =  zip(time_of_measurement, set_temp, resistance)
+
+# path to where file with given name is stored
+folder = Path("c:/sams/saved_data")
+date_today =  datetime.now().strftime("%Y_%m_%d_%H_%M_%S") #str(date.today()).replace('-','')
+fn = 'datalog_'+date_today+'_check_thermistor.txt'
+file_open = folder / fn
+
+# write to file
+with open(file_open, 'w', newline='') as f:
+    fieldnames = ['index', 'elapsed_time', 'time', 'temp']
+    data_writer = csv.DictWriter(data, fieldnames=fieldnames)
+    data_writer.writeheader()
+    for row in data:
+        data_writer.writerow({'time_of_measurement': data[0], 'set_temp':data[1], 'resistance':data[2] })
+
+   
 drywell.close()
 
 
--- instrument_control/check_thermometer.py
+++ instrument_control/check_thermometer.py
@@ -29,10 +29,12 @@ import sys
 
 
 ####### declare the instrument 
-# Connect to the multimeter
+
+## Connect to the multimeter
 rm = pyvisa.ResourceManager()
 dmm = rm.open_resource("GPIB0::21::INSTR") # check to see if this is true
-# Set the measurement
+
+## Set the measurement
 
 #rest the device
 dmm.write("*RST")
@@ -56,13 +58,15 @@ print('the mean resistance value is {}'.format(a))
 
 # path to where file with given name is stored
 folder = Path("c:/sams/saved_data")
-date =  datetime.datetime.now().strftime("%Y_%m_%d_%H_%M_%S") #str(date.today()).replace('-','')
+date =  datetime.now().strftime("%Y_%m_%d_%H_%M_%S") #str(date.today()).replace('-','')
 fn = 'datalog_'+date+'_check_thermistor.txt'
 file_open = folder / fn
 
 # function for recording temperature to file_open
 
-time_druation = 24*3600 # updae this number accordingly
+# num of hours
+hr = 2
+time_druation = hr*3600 # update this number accordingly
 
 # write a loop to measure over time frame covering the temperature experiment
 # log machine time, monotnic time and temperatuer and/or resistance 
@@ -70,7 +74,7 @@ time_druation = 24*3600 # updae this number accordingly
 
 # open file in write module
 
-def writer():
+def writer(time_duration, time_spacing=60):
     ''' this code'''
     with open(file_open, mode ='w', newline='') as data:
         fieldnames = ['index', 'elapsed_time', 'time', 'temp']
@@ -86,23 +90,9 @@ def writer():
             data_writer.writerow({'index':x,'elapsed_time':t_e,'time':time_stp, 'resistance':a})
             data.flush()# forces python to write to disk rather than writing to file in memory
             time.sleep(time_spacing) # wait 10 secon
-writer()
 
+writer(time_druation)
 
-'''
-
-def temp_recorder(time_duration, time_spacing = 60):
-    ''' time_duration: in secs, the total length of the measurement
-    time_spacing  is the sleep time between readings'''
-    for x in range(time_duration//2):
-        t_e = np.round((time.monotonic()-to), 2)
-        time_stp = time.monotonic()
-        a = np.fromstring((dmm.query(":READ?")).replace('\n',','), sep=',').mean()
-        data_writer.writerow({'index':x,'elapsed_time':t_e,'time':time_stp, 'resistance':a})
-        data.flush()# forces python to write to disk rather than writing to file in memory
-        time.sleep(time_spacing) # wait 10 secon
-
-'''
 
 
 
